5271383bd443898b30ec50fe5014edb804970e0d
Ash <shpadoinkle@hotmail.com>
2019-06-25 13:07:25 +1000

    web hosting changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Technical Test for Jumbo Interactive
 
-This can be viewed at [my firebase project](https://thoash-7b778.firebaseapp.com)
+This can be viewed at [my firebase project](https://thoash-jumbo.firebaseapp.com)
 
 
 you will need to run npm install, then npm run start to launch locally.
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
+import firebase from 'firebase';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './App.css';
@@ -13,18 +14,19 @@ import Movie from './screens/movie/Movie';
 
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   var config = {
-  //     apiKey: "sssss",
-  //     authDomain: "ssss",
-  //     databaseURL: "ssssss",
-  //     projectId: "a",
-  //     storageBucket: "a",
-  //     messagingSenderId: ""
-  //   };
-  //   firebase.initializeApp(config);
-  // }
+  constructor(props) {
+    super(props);
+    var config = {
+      apiKey: "AIzaSyDyUuuEHZA8WfuC5PhwVftvw9fUfENa5tM",
+      authDomain: "thoash-7b778.firebaseapp.com",
+      databaseURL: "https://thoash-7b778.firebaseio.com",
+      projectId: "thoash-7b778",
+      storageBucket: "thoash-7b778.appspot.com",
+      messagingSenderId: "213870389423",
+      appId: "1:213870389423:web:b75405ac6dab3002"
+    };
+    firebase.initializeApp(config);
+  }
 
   render() {
     const store = createStore(reducers, {});
